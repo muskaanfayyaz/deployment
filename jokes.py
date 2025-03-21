1100454d2684e35625f3a3fa28c0a8b74f668206
Muskaan Fayyaz <muskaanfayyaz3@gmail.com>
2025-03-22 00:40:32 +0500

    Deployments

---
+++ jokes.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import random
+
+app = FastAPI()
+
+jokes = [
+    "Why don't skeletons fight each other? They don't have the guts!",
+    "What do you call cheese that isn't yours? Nacho cheese!",
+    "Why couldn't the bicycle stand up by itself? It was two tired!",
+    "Parallel lines have so much in common. It’s a shame they’ll never meet.",
+    "Why do cows have hooves instead of feet? Because they lactose!"
+]
+
+@app.get("/randomjokes")
+def get_random_joke():
+    return {"joke": random.choice(jokes)}
